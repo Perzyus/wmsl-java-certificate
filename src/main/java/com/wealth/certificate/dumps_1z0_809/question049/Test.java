584ea11a16549bfa9f03efd842a43fe7545c9509
Saksiri Samranruen <Perzyuss@gmail.com>
2017-09-04 18:25:11 +0700

    no message

--- src/main/java/com/wealth/certificate/dumps_1z0_809/question049/Test.java
+++ src/main/java/com/wealth/certificate/dumps_1z0_809/question049/Test.java
@@ -7,21 +7,24 @@ import java.util.List;
 public class Test {
 
 	public static void main(String[] args) {
-		List<Integer> nums = Arrays.asList(10,20,8);
+		List<Integer> nums = Arrays.asList(5, 10,20,8);
 		//Comparator<Integer> comp = (p1, p2) -> Integer.compare(p1, p2);
 		System.out.println(
 				
 				/*A*/
-				nums.stream().max(Comparator.comparing(a -> a)).get()
+//				nums.stream().max(Comparator.comparing(a -> a)).get()
 				//or
-				//nums.stream().max(comp).get()
+//				nums.stream().max(comp).get()
 				
 				
-				/*B*///nums.stream().max(Integer::max).get() //Integer::max not a Comparator reference : https://www.programcreek.com/2014/01/why-stream-maxintegermax-compile/
+				/*B*/
+//				nums.stream().max(Integer::max).get() //Integer::max not a Comparator reference : https://www.programcreek.com/2014/01/why-stream-maxintegermax-compile/
 				
-				/*C*///nums.stream().max()  
+				/*C*/
+//				nums.stream().max()  
 				
-				/*D*///nums.stream().map(a -> a).max()
+				/*D*/
+//				nums.stream().map(a -> a).max()
 								
 		);
 		
